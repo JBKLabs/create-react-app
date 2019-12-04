13897ae3a5a6264382e338cd6b877d7bfbfd2e0c
Byron Hager <byron.g.hager@gmail.com>
2019-12-04 11:10:36 -0600

    HOTFIX: fix double quotes; update changelog

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 #!/usr/bin/env node
 
-const path = require("path");
-const git = require("simple-git")();
-const shell = require("shelljs");
-const tmp = require("tmp");
-const command = require("commander");
-const fs = require("fs");
-const pkg = require("./package.json");
-const inquirer = require("inquirer");
+const path = require('path');
+const git = require('simple-git')();
+const shell = require('shelljs');
+const tmp = require('tmp');
+const command = require('commander');
+const fs = require('fs');
+const pkg = require('./package.json');
+const inquirer = require('inquirer');
 
 let name;
-const templateRepo = "https://github.com/JBKLabs/example-react-project";
+const templateRepo = 'https://github.com/JBKLabs/example-react-project';
 
 const run = (message, cb) => (...args) =>
   new Promise(async resolve => {
     console.log(`${message} ...`);
     cb(...args, result => {
-      console.log("\tdone.");
+      console.log('\tdone.');
       resolve(result);
     });
   });
 
 const createProjectDirectory = name => {
-  if (name !== ".") {
+  if (name !== '.') {
     try {
       fs.mkdirSync(name);
     } catch (error) {
-      if (error.code === "EEXIST") {
+      if (error.code === 'EEXIST') {
         throw new Error(
           `That directory already exists. Try running from within ${name}/ as 'create-react-app .'`
         );
@@ -37,7 +37,7 @@ const createProjectDirectory = name => {
   }
 };
 
-const cloneExampleProject = run("cloning example project", cb => {
+const cloneExampleProject = run('cloning example project', cb => {
   tmp.dir({ unsafeCleanup: true }, (err, tmpPath, cleanupCallback) => {
     if (err) {
       throw err;
@@ -65,10 +65,10 @@ const cloneExampleProject = run("cloning example project", cb => {
 
 const replaceToken = (tempPath, [key, value]) => {
   if (key && value) {
-    shell.ls("-RA", `${tempPath}`).forEach(file => {
+    shell.ls('-RA', `${tempPath}`).forEach(file => {
       try {
         shell.sed(
-          "-i",
+          '-i',
           `<% ${key} %>`,
           value.toString(),
           path.join(tempPath, file)
@@ -79,29 +79,29 @@ const replaceToken = (tempPath, [key, value]) => {
 };
 
 const renderTemplate = run(
-  "rendering template",
+  'rendering template',
   (template, projectPath, options, cb) => {
-    shell.rm("-rf", path.join(template.path, "./.git"));
+    shell.rm('-rf', path.join(template.path, './.git'));
     Object.entries(options).forEach(o => replaceToken(template.path, o));
-    shell.cp("-rf", `${template.path}/*`, projectPath);
-    shell.cp("-rf", `${template.path}/.*`, projectPath);
+    shell.cp('-rf', `${template.path}/*`, projectPath);
+    shell.cp('-rf', `${template.path}/.*`, projectPath);
     template.clear();
     cb();
   }
 );
 
-const prepareProject = run("preparing project", (projectPath, options, cb) => {
+const prepareProject = run('preparing project', (projectPath, options, cb) => {
   shell.cd(projectPath);
 
-  if (options.initializeGit && shell.exec("git init").code !== 0) {
-    shell.echo("Error: git init failed");
+  if (options.initializeGit && shell.exec('git init').code !== 0) {
+    shell.echo('Error: git init failed');
     shell.exit(1);
   }
 
-  console.log("installing dependencies ...");
+  console.log('installing dependencies ...');
 
-  if (shell.exec("npm i").code !== 0) {
-    shell.echo("Error: npm install failed");
+  if (shell.exec('npm i').code !== 0) {
+    shell.echo('Error: npm install failed');
     shell.exit(1);
   }
 
@@ -113,9 +113,9 @@ const promptUser = projectPath =>
     const gitPrompt = !fs.existsSync(`${projectPath}/.git`)
       ? [
           {
-            type: "confirm",
-            message: "Would you like to initialize git?",
-            name: "initializeGit"
+            type: 'confirm',
+            message: 'Would you like to initialize git?',
+            name: 'initializeGit'
           }
         ]
       : [];
@@ -123,9 +123,9 @@ const promptUser = projectPath =>
     inquirer
       .prompt([
         {
-          type: "input",
-          message: "Enter your project name:",
-          name: "projectName"
+          type: 'input',
+          message: 'Enter your project name:',
+          name: 'projectName'
         },
         ...gitPrompt
       ])
@@ -134,11 +134,11 @@ const promptUser = projectPath =>
 
 command
   .version(pkg.version)
-  .arguments("<directoryName>")
-  .option("-b, --branch [branch]", "example-react-project branch name")
+  .arguments('<directoryName>')
+  .option('-b, --branch [branch]', 'example-react-project branch name')
   .action(async directoryName => {
     name = directoryName;
-    const projectPath = name ? `./${name}` : ".";
+    const projectPath = name ? `./${name}` : '.';
     createProjectDirectory(name);
     const options = await promptUser(projectPath);
     const template = await cloneExampleProject();
@@ -148,7 +148,7 @@ command
 
 command.parse(process.argv);
 
-if (typeof name === "undefined") {
-  console.error("Directory name is required.");
+if (typeof name === 'undefined') {
+  console.error('Directory name is required.');
   process.exit(1);
 }
